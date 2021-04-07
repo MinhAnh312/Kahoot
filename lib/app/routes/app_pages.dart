58f6b9ea12d922a2fc4932b23af838ec26e73696
MinhAnh312 <56524674+MinhAnh312@users.noreply.github.com>
2021-04-07 18:41:01 +0700

    Add files via upload

---
+++ lib/app/routes/app_pages.dart
@@ -0,0 +1,44 @@
+
+import '../../app/modules/question_module/question_page.dart';
+import '../../app/modules/question_module/question_bindings.dart';
+import '../../app/modules/entername_module/entername_page.dart';
+import '../../app/modules/entername_module/entername_bindings.dart';
+import '../../app/modules/enterroom_module/enterroom_page.dart';
+import '../../app/modules/enterroom_module/enterroom_bindings.dart';
+import '../../app/modules/home_module/home_page.dart';
+import '../../app/modules/home_module/home_bindings.dart';
+import '../../app/modules/login_module/login_bindings.dart';
+import '../../app/modules/login_module/login_page.dart';
+import 'package:get/get.dart';
+part './app_routes.dart';
+
+
+abstract class AppPages {
+  static final pages = [
+    GetPage(
+      name: Routes.LOGIN,
+      page: () => LoginPage(),
+      binding: LoginBinding(),
+    ),
+    GetPage(
+      name: Routes.HOME,
+      page: () => HomePage(),
+      binding: HomeBinding(),
+    ),
+    GetPage(
+      name: Routes.ENTERROOM,
+      page: () => EnterRoomPage(),
+      binding: EnterRoomBinding(),
+    ),
+    GetPage(
+      name: Routes.ENTERNAME,
+      page: () => EnterNamePage(),
+      binding: EnterNameBinding(),
+    ),
+    GetPage(
+      name: Routes.QUESTION,
+      page: () => QuestionPage(),
+      binding: QuestionBinding(),
+    ),
+  ];
+}
---
+++ lib/app/routes/app_routes.dart
@@ -0,0 +1,11 @@
+part of './app_pages.dart';
+
+
+abstract class Routes {
+  static const LOGIN = '/login'; // Login page
+  static const HOME = '/home'; // Home page
+  static const ENTERROOM = '/enterroom'; // EnterRoom page
+  static const ENTERNAME = '/entername'; // EnterName page
+  static const QUESTION = '/question'; // Question page
+  static const MAP = '/map'; // map page
+}
