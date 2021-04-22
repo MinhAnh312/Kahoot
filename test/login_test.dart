6475669fc0ddbff2cf7fb5537753da10cb691727
dat15021999 <43447012+dat15021999@users.noreply.github.com>
2021-04-22 13:22:34 +0700

    update login_test

--- test/login_test.dart
+++ test/login_test.dart
@@ -6,16 +6,51 @@ void main() {
   group('Test Login', () {
     test('Login success', () async {
       await _login.loginInEmail('ltthinhtb@gmail.com', '123456');
-      expect(_login.msg, 'Sucess');
+      expect(_login.msg, 'Thành công');
     });
 
     test('Login success', () async {
+      await _login.loginInEmail('tthinhtb@gmail.com', '123456');
+      expect(_login.msg, 'Thành công');
+    });
+
+    test('Login fail', () async {
       await _login.loginInEmail('ltth@gmail.com', '123456');
       expect(_login.msg, 'Tài khoản không tồn tại');
     });
 
     test('Login success', () async {
+      await _login.loginInEmail('ltfdsfth@gmail.com', '123456');
+      expect(_login.msg, 'Tài khoản không tồn tại');
+    });
+
+    test('Login fail', () async {
+      await _login.loginInEmail('ltth@gmail.com', '123456');
+      expect(_login.msg, 'Tài khoản không tồn tại');
+    });
+
+    test('Login success', () async {
+      await _login.loginInEmail('ltfdsfth@gmail.com', '123456');
+      expect(_login.msg, 'Tài khoản không tồn tại');
+    });
+
+    test('Login success', () async {
       await _login.loginInEmail('ltthinhtb@gmail.com', '1221213456');
+      expect(_login.msg, 'Sai mật khẩu');
+    });
+
+    test('Login success', () async {
+      await _login.loginInEmail('ltth@gmail.com', '123456');
+      expect(_login.msg, 'Sai mật khẩu');
+    });
+
+    test('Login success', () async {
+      await _login.loginInEmail('ltthinhtb@gmail.com', '1221213456');
+      expect(_login.msg, 'Sai mật khẩu');
+    });
+
+    test('Login success', () async {
+      await _login.loginInEmail('ltth@gmail.com', '123456');
       expect(_login.msg, 'Sai mật khẩu');
     });
   });
